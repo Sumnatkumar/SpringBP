64a65206faf37a329cd5c7d2f5c2fe96197b7f3f
Sumant  kumar <135256285+Sumnatkumar@users.noreply.github.com>
2025-02-03 17:19:45 +0530

    tst

---
+++ myappvs/src/main/java/myappvs/myappvs/MyappvsApplication.java
@@ -0,0 +1,14 @@
+package myappvs.myappvs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyappvsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyappvsApplication.class, args);
+		System.out.println("started");
+	}
+
+}
---
+++ myappvs/src/test/java/myappvs/myappvs/MyappvsApplicationTests.java
@@ -0,0 +1,13 @@
+package myappvs.myappvs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyappvsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
